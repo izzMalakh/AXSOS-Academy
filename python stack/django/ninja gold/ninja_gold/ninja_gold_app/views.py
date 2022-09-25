0310829473b51a3791a59d06d72e31bc6aa1f144
izzMalakh <izzaldeenjamal@gmail.com>
2022-09-25 12:56:15 +0300

    -

--- python stack/django/ninja gold/ninja_gold/ninja_gold_app/views.py
+++ python stack/django/ninja gold/ninja_gold/ninja_gold_app/views.py
@@ -6,6 +6,9 @@ def method(request):
         request.session['count']= 0
     if 'activities' not in request.session:
         request.session['activities'] = []
+    if 'x' not in request.session:
+        request.session['x'] = 0
+    
        
 
     return render(request,"ninjagame.html")
@@ -15,23 +18,42 @@ def process_money(request):
     if request.POST['button'] == "farm":
         gold = random.randint(10,20)
         request.session['count'] += gold
+        request.session['x'] = 0
         activity = "You entered a farm and earn " + str(gold) +"gold" + str(now) 
         request.session['activities'].append(activity)
+
     elif request.POST['button'] == "cave":
         gold = random.randint(10,20)
         request.session['count'] += gold
+        request.session['x'] = 0
         activity = "You entered a cave and earn " + str(gold) +"gold" + " "+ str(now) 
         request.session['activities'].append(activity)
+
     elif request.POST['button'] == "house":
         gold = random.randint(10,20)
         request.session['count'] += gold
+        request.session['x'] = 0
         activity = "You entered a house and earn " + str(gold) +"gold" + str(now)
         request.session['activities'].append(activity)
+
     elif request.POST['button'] == "quest":
-        gold = random.randint(0,50)
-        request.session['count'] += gold
-        activity = "You entered a quest and earn " + str(gold) +"gold" + str(now)
-        request.session['activities'].append(activity)
+        
+        gold = random.randint(-50,50)
+        if gold < 0: 
+            request.session['count'] -= 50
+            x = -1*gold 
+            request.session['x'] += 1
+
+            activity = "You faild a quest and lost " + str(x) +"gold" + str(now)
+            request.session['activities'].append(activity)
+            
+        elif gold >0:
+            request.session['count'] += 50
+            request.session['x'] = 0
+            activity = "Nice You entered a quest and earn " + str(gold) +"gold" + str(now)
+            
+            request.session['activities'].append(activity)
+
     return redirect('/')
 
 def reset(request):
