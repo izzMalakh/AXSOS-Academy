2f41da7b464791973dbc6ee5f17da410d5a9185c
Izzaldeen Jamal Malakh <111282612+izzMalakh@users.noreply.github.com>
2022-10-28 14:48:27 +0200

    Delete python stack directory

--- python stack/django/dojo survey by django/firstapp/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', view=views.method1),
-    path('show', view=views.showform)
-]
--- python stack/django/great_num_game/great_number_game/great_number_game_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GreatNumberGameAppConfig(AppConfig):
-    name = 'great_number_game_app'
